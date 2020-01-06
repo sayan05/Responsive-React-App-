dcf68ad8fdf9f8b94addc7199a163f4a9cf3fdbd
Sayan Ganguly <amisayan05@gmail.com>
2020-01-06 10:22:35 +0530

    Updates

---
+++ precache-manifest.53f53157f7b899658a87489a244bc318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2507446f4f1a88f0ee80e42d384866a5",
+    "url": "/Responsive-React-App-/index.html"
+  },
+  {
+    "revision": "036c2e96f19f49df9105",
+    "url": "/Responsive-React-App-/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6872496376da41f03c15",
+    "url": "/Responsive-React-App-/static/css/main.2c9b14dd.chunk.css"
+  },
+  {
+    "revision": "036c2e96f19f49df9105",
+    "url": "/Responsive-React-App-/static/js/2.a3d4db76.chunk.js"
+  },
+  {
+    "revision": "6872496376da41f03c15",
+    "url": "/Responsive-React-App-/static/js/main.6f9b860e.chunk.js"
+  },
+  {
+    "revision": "aa09e556e6a290fa9455",
+    "url": "/Responsive-React-App-/static/js/runtime-main.a5ad691c.js"
+  }
+]);
